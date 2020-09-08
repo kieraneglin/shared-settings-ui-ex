0afd66875da79606fc9c622c568403ae86dc6639
Kieran Eglin <kieran.eglin@gmail.com>
2020-09-08 09:24:48 -0700

    Added application callbacks for use in other plug apps

--- lib/shared_settings/ui.ex
+++ lib/shared_settings/ui.ex
@@ -1,7 +1,31 @@
 defmodule SharedSettings.UI do
-  @spec run_standalone :: {:error, any} | {:ok, pid}
+  use Application
+
+  @doc false
+  def start(_type, _args) do
+    check_cowboy()
+
+    children = [
+      Plug.Adapters.Cowboy.child_spec(:http, FunWithFlags.UI.Router, [], [port: 8080])
+    ]
+
+    opts = [strategy: :one_for_one, name: FunWithFlags.UI.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
   def run_standalone do
     Plug.Adapters.Cowboy.http SharedSettings.UI.Router, [], port: 4005
+  end
+
+  defp check_cowboy do
+    with :ok <- Application.ensure_started(:ranch),
+         :ok <- Application.ensure_started(:cowlib),
+         :ok <- Application.ensure_started(:cowboy) do
+      :ok
+    else
+      {:error, _} ->
+        raise "You need to add :cowboy to your Mix dependencies to run FunWithFlags.UI standalone."
+    end
   end
 
   defimpl Jason.Encoder, for: [SharedSettings.Setting] do
